791511b0ed36a913ae594b7627dbf4b3abc331e1
kaka <lanyu@sensor-music.com>
2013-06-04 12:47:21 +0800

    Initial Commit

---
+++ SmartBat/BTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BTAppDelegate.h
+//  SmartBat
+//
+//  Created by kaka' on 13-6-4.
+//  Copyright (c) 2013年 kaka'. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartBat/BTViewController.m
@@ -0,0 +1,29 @@
+//
+//  BTViewController.m
+//  SmartBat
+//
+//  Created by kaka' on 13-6-4.
+//  Copyright (c) 2013年 kaka'. All rights reserved.
+//
+
+#import "BTViewController.h"
+
+@interface BTViewController ()
+
+@end
+
+@implementation BTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
