d298baabcaadfcaf41a2351a9031b75e41d2aafa
lanyu7 <lanyu7@live.com>
2013-08-01 17:16:30 +0800

    转移文件夹

--- csr/demo/depend_Release_CSR100x/app_gatt_db.db.d
+++
@@ -1,1 +0,0 @@
-depend_Release_CSR100x/app_gatt_db.db_: app_gatt_db.db smart_band.h
--- csr/release/depend_Release_CSR100x/app_gatt_db.db.d
+++
@@ -1,3 +0,0 @@
-depend_Release_CSR100x/app_gatt_db.db_: app_gatt_db.db gap_service_db.db \
-  gap_uuids.h appearance.h gap_conn_params.h dev_info_service_db.db \
-  dev_info_uuids.h gatt_service_db.db
