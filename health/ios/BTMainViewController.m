2977e00b7e4ecc0b6e2f34dac3a6cd2fc3685e98
poppyrr <poppyrr@gmail.com>
2013-10-22 16:15:13 +0800

    去掉勿用代码

--- health/ios/BTMainViewController.m
+++ health/ios/BTMainViewController.m
@@ -25,6 +25,8 @@
     
     self.dailyData = [[NSMutableArray alloc] initWithCapacity:24];
     
+    //[self updateValue: 50];
+    
 }
 
 //监控参数，更新显示
@@ -125,16 +127,19 @@
     
     [graphView setArray:points];
     
-    [self updateValue: 50];
-    
     
 }
 
 -(void) updateValue: (float) value
 {
     
-    self.sportNum.text = [NSString stringWithFormat:@"%f.0",value];
-    self.sportLevel.frame.origin.y = 91 - value;
+//    CGContextRef context = UIGraphicsGetCurrentContext();
+//    CGContextSetRGBStrokeColor(context,1,1,1,1.0);//画笔线的颜色
+//    CGContextSetLineWidth(context, 1.0);//线的宽度
+//    //void CGContextAddArc(CGContextRef c,CGFloat x, CGFloat y,CGFloat radius,CGFloat startAngle,CGFloat endAngle, int clockwise)1弧度＝180°/π （≈57.3°） 度＝弧度×180°/π 360°＝360×π/180 ＝2π 弧度
+//    // x,y为圆点坐标，radius半径，startAngle为开始的弧度，endAngle为 结束的弧度，clockwise 0为顺时针，1为逆时针。
+//    CGContextAddArc(context, 100, 20, 15, 0, 2*PI, 0); //添加一个圆
+//    CGContextDrawPath(context, kCGPathStroke); //绘制路径
     
     
 }
